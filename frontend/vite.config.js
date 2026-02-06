c7f33bbeabfc244d80b754a13fb91f1cdb0100a2
igihozonelly3 <igihozonelly3@gmail.com>
2026-02-06 17:10:16 +0200

    Fix: Add dedupe to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: 
-  [
+  plugins: [
     react(),
     tailwindcss()
-
   ],
-  
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
 })
